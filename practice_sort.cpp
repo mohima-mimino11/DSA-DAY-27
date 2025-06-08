f293f7fbb6f51e8ef3ace08889293c36bbc4360a
mohima-mimino11 <mohimahoque22@gmail.com>
2025-06-08 18:44:54 +0600

    practice sorting added

--- practice_sort.cpp
+++ practice_sort.cpp
@@ -6,20 +6,19 @@ class Student{
   int roll;
   int marks;
 };
+bool cmp(Student l, Student r){
+  return (l.marks == r.marks) ? l.roll > r.roll : l.marks < r.marks;
+}
 int main(){
   int n;
   cin >> n;
   Student a[n];
   for(int i = 0; i < n; i++){
-    cin.ignore();
-    getline(cin, a[i].name);
-    cin >> a[i].roll >> a[i].marks;
+    cin >> a[i].name >> a[i].roll >> a[i].marks;
   }
+  sort(a, a+n, cmp);
   for(int i = 0; i < n; i++){
     cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
   }
-  // cout << a[2].name << " " << a[2].roll << " " << a[2].marks << endl;
-
-
   return 0;
 }
